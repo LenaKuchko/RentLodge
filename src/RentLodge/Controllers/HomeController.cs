8a083223aedaaa1a49975ba47e3629b3eb741421
Olena Kuchko <kuchkoel@gmail.com>
2017-08-27 16:32:58 -0700

    add styling to home page

--- src/RentLodge/Controllers/HomeController.cs
+++ src/RentLodge/Controllers/HomeController.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using RentLodge.Data;
+using RentLodge.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace RentLodge.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public HomeController( ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            IEnumerable<Country> allCountries = this._db.Countries.ToList();
+            ViewBag.Countries = new SelectList(allCountries, "Name", "Name");
             return View();
         }
 
