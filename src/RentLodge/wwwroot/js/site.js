bdd98b26a7fc80e1c407ed7064e502ddbe85c21c
Olena Kuchko <kuchkoel@gmail.com>
2017-08-27 10:32:53 -0700

    add materialize

--- src/RentLodge/wwwroot/js/site.js
+++ src/RentLodge/wwwroot/js/site.js
@@ -1,1 +1,13 @@
-﻿// Write your Javascript code.
+﻿$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15, // Creates a dropdown of 15 years to control year,
+    today: 'Today',
+    clear: 'Clear',
+    close: 'Ok',
+    closeOnSelect: false // Close upon selecting a date,
+});
+
+// Initialize collapse button
+$(".button-collapse").sideNav();
+// Initialize collapsible (uncomment the line below if you use the dropdown variation)
+//$('.collapsible').collapsible();
