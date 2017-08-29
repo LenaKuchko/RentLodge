f1d8efbc3281e5e5429b458a2a91743bbde2cbee
Olena Kuchko <kuchkoel@gmail.com>
2017-08-29 16:39:34 -0700

    add Lato font

--- src/RentLodge/wwwroot/js/site.js
+++ src/RentLodge/wwwroot/js/site.js
@@ -1,11 +1,4 @@
-﻿$('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15, // Creates a dropdown of 15 years to control year,
-    today: 'Today',
-    clear: 'Clear',
-    close: 'Ok',
-    closeOnSelect: false // Close upon selecting a date,
-});
+﻿
 
 // Initialize collapse button
 $(".button-collapse").sideNav();
